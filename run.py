d4b698cbd9f265de2991f2a1148826d1ca7b4620
unknown <marzakanass2@gmail.com>
2026-05-11 12:23:49 +0100

    Fix: Define app instance for Gunicorn

--- run.py
+++ run.py
@@ -96,6 +96,8 @@ def create_app():
     return app
 
 
+# Export the Flask application for gunicorn/WSGI servers
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="127.0.0.1", port=5050, debug=True)
